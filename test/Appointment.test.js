f90d456d8eb626f43c570515f8d55762ba3b58d2
ryan cho <ryancho@cizion.com>
2019-07-01 13:54:32 +0900

    first test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,3 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Appointment } from "../src/Appointment";
+
 describe("Appointment", () => {
-  it("renders the customer first name", () => {});
+  it("renders the customer first name", () => {
+    const customer = { firstName: "Ashely" };
+    const container = document.createElement("div");
+    ReactDom.render(<Appointment customer={customer} />, container);
+
+    expect(container.textContent).toMatch("Ashely");
+  });
+
+  it("renders the another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    const container = document.createElement("div");
+    ReactDom.render(<Appointment customer={customer} />, container);
+
+    expect(container.textContent).toMatch("Jordan");
+  });
 });
